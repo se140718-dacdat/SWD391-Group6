0450f33b3ee54121cb367ca8ddfc6f61f7da60bd
se140718 <datlhdse140718@fpt.edu.vn>
2022-07-10 01:37:05 +0700

    student api

--- backend/controller/studentController.js
+++ backend/controller/studentController.js
@@ -12,6 +12,14 @@ const studentController = {
         } catch (err) {
             res.status(500).json("Not found!");
         }
+    },
+    getAllStudent: async(req, res) => {
+        try {
+            const students = await Student.find();
+            res.status(200).json(students);
+        } catch (err) {
+            res.status(500).json(error);
+        }
     }
 }
 
--- backend/routes/student.js
+++ backend/routes/student.js
@@ -3,6 +3,6 @@ const studentController = require('../controller/studentController');
 const router = require('express').Router();
 
 
+router.get('/get-all-student', studentController.getAllStudent);
 router.get('/:username', studentController.getStudent);
-
 module.exports = router;
