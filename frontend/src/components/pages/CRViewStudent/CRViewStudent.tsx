0fbfaa754f550f7f964140101694d27a10d553e1
Long <longptse140353@gmail.com>
2022-07-15 00:42:39 +0700

    fix table layout

--- frontend/src/components/pages/CRViewStudent/CRViewStudent.tsx
+++ frontend/src/components/pages/CRViewStudent/CRViewStudent.tsx
@@ -3,28 +3,30 @@ import { NavLink } from "react-router-dom";
 import { Student } from "../../../model";
 import "./CRViewStudent.css";
 import { studentList } from "../../../data.js";
+import "primeicons/primeicons.css";
 
 const CRViewStudent = () => {
   //   console.log(studentList);
-  const [students, setStudent] = useState<Student[]>([]);
+  const [students, setStudent] = useState([studentList]);
+  console.log(students[0][0].fullName);
   return (
     <div id="CRViewStudent">
       <div id="content">
         <div className="search-bar">
-          <i className="fas fa-search search--bar--icon search-icon"></i>
+          <i className="pi pi-search search--bar--icon search-icon"></i>
           <input
             type="text"
             className="search-input"
             placeholder="Search student's id"
           />
-          <i className="fas fa-times-circle clear-icon"></i>
+          <i className="pi pi-times-circle clear-icon"></i>
           <button className="btn btn-search">
-            <i className="fas fa-search search-icon"></i> Search
+            <i className="pi pi-search search-icon"></i> Search
           </button>
         </div>
         <div className="filter">
           <div className="majors filter--item filter--select">
-            Majors <i className="fas fa-sort-down"></i>
+            Majors <i className="pi pi-sort-down"></i>
             <div id="majors-dropdown">
               <option className="majors-item" value="All">
                 All
@@ -35,32 +37,29 @@ const CRViewStudent = () => {
           <div>
             <table className="table">
               <tr className="table-title">
-                <td>No.</td>
-                <td>Roll Number</td>
-                <td>Student's Name</td>
-                <td>Other</td>
+                <th>No.</th>
+                <th>Roll Number</th>
+                <th>Student's Name</th>
+                <th>Other</th>
               </tr>
-              {/* {
-                                students.map(item => */}
-              {/* [ */}
-              <tr className="item">
-                <td rowSpan={2}>SE140718</td>
-                <td rowSpan={2}>Luong Ho Dac Dat</td>
-                <td>
-                  <button className="details-button">Details</button>
-                </td>
-              </tr>
-              {/* ] */}
-              {/* )
-                            } */}
+              {students[0].map((item, index) => [
+                <tr className="item">
+                  <td rowSpan={2}>{index + 1}</td>
+                  <td rowSpan={2}>{item.studentID}</td>
+                  <td rowSpan={2}>{item.fullName}</td>
+                  <td>
+                    <button className="details-button">Details</button>
+                  </td>
+                </tr>,
+              ])}
             </table>
             <div className="paging">
               <button className="paging-btn pre-btn">
-                <i className="fas fa-angle-double-left"></i>
+                <i className="pi pi-angle-double-left"></i>
               </button>
               <div className="page-number">1</div>
               <button className="paging-btn next-btn">
-                <i className="fas fa-angle-double-right"></i>
+                <i className="pi pi-angle-double-right"></i>
               </button>
             </div>
           </div>
