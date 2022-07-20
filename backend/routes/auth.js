e65cc5175ff1fafaebc602218873b1a6e95faae6
Long <longptse140353@gmail.com>
2022-07-20 12:01:56 +0700

    fix layout

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,12 @@
-const router = require('express').Router();
-const authController = require('../controller/authController');
-const middlewareController = require('../controller/middlewareController');
+const router = require("express").Router();
+const authController = require("../controller/authController");
+const middlewareController = require("../controller/middlewareController");
 router.post("/register", authController.registerUser);
 router.post("/login", authController.loginUser);
-router.post("/logout", middlewareController.verifyToken, authController.userLogout);
+router.post(
+  "/logout",
+  middlewareController.verifyToken,
+  authController.userLogout
+);
 router.post("/refresh", authController.refreshToken);
 module.exports = router;
